df4a463df56ffd4686312a9b3d1536ce6470aac1
Kevin Tan <kevin@ktizzel.com>
2016-09-26 15:07:03 -0400

    fb login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
 
-const responseFacebook = (response) => {
-  console.log(response);
+// const responseFacebook = (response) => {
+//   console.log(response);
+//   var provider = new firebase.auth.FacebookAuthProvider();
+// }
+
+class LoginButton extends Component {
+  login = () => {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('name,image,public_profile,email,user_friends');
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user)
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log(error)
+      // ...
+    });
+
+  }
+  render(){
+      return(
+        <button onClick={this.login}>Facebook Login</button>
+      )
+  }
 }
+// <FacebookLogin
+//   appId="180398319064354"
+//   autoLoad={true}
+//   scope="name,email,picture,public_profile,user_friends"
+//   callback={responseFacebook}
+//   cssClass="fb-login-button"
+//   icon="fa-facebook"
+// />
 
 const Container = ({ }) => {
   return (
-    <FacebookLogin
-      appId="180398319064354"
-      autoLoad={true}
-      fields="name,email,picture,public_profile,user_friends"
-      callback={responseFacebook}
-      cssClass="fb-login-button"
-      icon="fa-facebook"
-    />
+    <LoginButton/>
   )
 }
 export default Container
