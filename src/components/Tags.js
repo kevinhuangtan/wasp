eb7b1f4d678370f5d07eaadff013be641472234d
Kevin Tan <kevin@ktizzel.com>
2016-10-04 12:10:23 -0400

    new commit

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -65,7 +65,6 @@ class TagsContainer extends Component {
                 width: 300,
                 fontSize: 14,
                 marginTop: 10,
-                margin: '0 auto'
               }}>
               <ReactTags
                 tags={tagsSelectedTransform}
