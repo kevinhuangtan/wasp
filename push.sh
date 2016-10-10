3d0ca739e8bcbaf55051a3fbd5b433c749df368c
Kevin Tan <kevin@ktizzel.com>
2016-10-10 03:22:19 -0400

    new commit

--- push.sh
+++ push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echo enter commit message
+read COMMIT_MESSAGE
 echo "npm run build"
 npm run build
 
 echo "git add -A"
 git add -A
 
-echo "git commit -m 'new 'commit'"
+echo "git commit -m '$COMMIT_MESSAGE'"
 git commit -m "new commit"
 
 echo "git push heroku master"
